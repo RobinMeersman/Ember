bd11f654d95b05ba40879ed097a3c1ec643a7395
Robin Meersman <robin@pop-os.localdomain>
2023-09-16 14:56:11 +0200

    feat: gui

---
+++ gui/views/SearchBar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by robin on 9/14/23.
+//
+
+#include "SearchBar.h"
---
+++ gui/views/SearchBar.h
@@ -0,0 +1,14 @@
+//
+// Created by robin on 9/14/23.
+//
+
+#ifndef EMBER_SEARCHBAR_H
+#define EMBER_SEARCHBAR_H
+
+
+class SearchBar {
+
+};
+
+
+#endif //EMBER_SEARCHBAR_H
